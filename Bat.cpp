c63b885034851da757ff361bf1ab01207a37cb75
Matthew Ryan Granger <mattg1827@gmail.com>
2025-01-09 23:41:36 -0800

    Implementing Bat.h in Bat.cpp

--- Bat.cpp
+++ Bat.cpp
@@ -5,3 +5,47 @@ Bat::Bat(float startX, float startY): m_Position(startX, startY)
   m_Shape.setSize(sf::Vector2f(50,5));
   m_Shape.setPosition(m_Position);
 }
+
+sf::FloatRect Bat::getPosition() const
+{
+  return m_Shape.getGlobalBounds();
+}
+
+sf::RectangleShape Bat::getShape() const
+{
+  return m_Shape;
+}
+
+void Bat::moveLeft()
+{
+  m_Moving_Left = true;
+}
+
+void Bat::moveRight()
+{
+  m_Moving_Right = true;
+}
+
+void Bat::stopLeft()
+{
+  m_Moving_Left = false;
+}
+
+void Bat::stopRight()
+{
+  m_Moving_Right = false;
+}
+void Bat::update(sf::Time dt)
+{
+  if (m_Moving_Left)
+  {
+    m_Position.x -= m_Speed * dt.asSeconds();
+  }
+  if (m_Moving_Right)
+  {
+    m_Position.x += m_Speed * dt.asSeconds();
+  }
+  m_Shape.setPosition(m_Position);
+}
+
+
